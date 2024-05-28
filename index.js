bd72425b3da8bc41da378ee53dd6c6a080c580a4
Francisco Daniel Gregorio Guevara <160035631+FRANCISCODANIEL03@users.noreply.github.com>
2024-05-28 08:34:01 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -11,94 +11,107 @@ app.use(cors());
 //Iniciamos el servidor
 const PORT = 3000;
 
-app.listen(PORT,()=>{console.log("Servidor corriendo en http://localhost:"+PORT);});
+app.listen(PORT, () => { console.log("Servidor corriendo en http://localhost:" + PORT); });
 
 //conexion con mysql
 const conection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"FRANCISCO031147",
-    port : 3306,
-    database: "topicos"
+    host: "roundhouse.proxy.rlwy.net",
+    user: "root",
+    password: "ihIQNmQEjowdXvqvsadUNsbaOtbgYaRZ",
+    port: 11642,
+    database: "railway"
 });
 
-conection.connect((err)=>{
-    if(err){
+conection.connect((err) => {
+    if (err) {
         console.error(err.message || "No se pudo conectar a la base de datos");
     }
-    else{
+    else {
         console.log("Conectado a la base de datos");
     }
 });
 //obtener los datos de la BD
-app.get("/",(req,res)=>{
-    conection.query("SELECT * FROM usuarios",(error, results)=>{
-        if(error){ res.status(500).json({message: error.message || "No se puede obtener datos en este momento"+ 
-        "para la tabla usuarios"});}
+app.get("/", (req, res) => {
+    conection.query("SELECT * FROM usuarios", (error, results) => {
+        if (error) {
+            res.status(500).json({
+                message: error.message || "No se puede obtener datos en este momento" +
+                    "para la tabla usuarios"
+            });
+        }
         else res.status(200).json(results);
     });
 });
 //insertar datos a la BD
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
     var { nombre } = req.body;
-    if(nombre == ""){
+    if (nombre == "") {
         res.json({
-            message : "Inserta un nombre"
+            message: "Inserta un nombre",
+            value: 2
         });
-    }else{
-    conection.query(`INSERT INTO usuarios  (nombre) VALUES (?)`,[nombre],
-    (error,results)=>{
-        if(error){
-         res.status(500).json({
-         message: error.message || "No se puede hacer la insercion de datos"});
-        } else {res.status(200).json({
-            message: "Datos insertados correctamente",
-            data: results});
-        }    
-    });
+    } else {
+        conection.query(`INSERT INTO usuarios  (nombre) VALUES (?)`, [nombre],
+            (error, results) => {
+                if (error) {
+                    res.status(500).json({
+                        message: error.message || "No se puede hacer la insercion de datos"
+                    });
+                } else {
+                    res.status(200).json({
+                        message: "Usuario insertado correctamente",
+                        data: results,
+                        value: 1
+                    });
+                }
+            });
     }
 });
 //actualizar datos de la BD
 app.patch("/", (req, res) => {
-    const { id,nombre } = req.body;
-    if(id == "" || nombre == ""){
+    const { id, nombre } = req.body;
+    if (id == "" || nombre == "") {
         res.json({
-            message : "Algun campo esta vacio"
+            message: "Algun campo esta vacio",
+            value: 2
         });
-    }else{
-    conection.query(`UPDATE usuarios SET nombre = ? WHERE id = ?`, [nombre, id], (error, results) => {
-        if (error) {
-            res.status(500).json({
-                message: error.message || "No se puede actualizar el usuario"
-            });
-        } else {
-            res.status(200).json({
-                message: "Usuario actualizado correctamente",
-                data: results
-            });
-        }
-    });
+    } else {
+        conection.query(`UPDATE usuarios SET nombre = ? WHERE id = ?`, [nombre, id], (error, results) => {
+            if (error) {
+                res.status(500).json({
+                    message: error.message || "No se puede actualizar el usuario"
+                });
+            } else {
+                res.status(200).json({
+                    message: "Usuario actualizado correctamente",
+                    value: 1,
+                    data: results
+                });
+            }
+        });
     }
 });
 //Eliminar datos de la BD
 app.delete("/", (req, res) => {
     const { id } = req.body;
-    if(id == ""){
+    if (id == "") {
         res.json({
-            message : "Inserta un id"
+            message: "Inserta un id",
+            value: 2
         });
-    }else{
-    conection.query(`DELETE FROM usuarios WHERE id = ?`, [id], (error, results) => {
-        if (error) {
-            res.status(500).json({
-                message: error.message || "No se puede eliminar el usuario"
-            });
-        } else {
-            res.status(200).json({
-                message: "Usuario eliminado correctamente",
-                data: results
-            });
-        }
-    });
+    } else {
+        conection.query(`DELETE FROM usuarios WHERE id = ?`, [id], (error, results) => {
+            if (error) {
+                res.status(500).json({
+                    message: error.message || "No se puede eliminar el usuario"
+                });
+            } else {
+                res.status(200).json({
+                    message: "Usuario eliminado correctamente",
+                    value: 1,
+                    data: results
+                });
+            }
+        });
     }
 });
